2ef852c76c5074672fabc46e48a38809eeaa77f0
ba-na-na-na-na <vojko.hysz@gmail.com>
2023-02-21 18:23:50 +0100

    Slika in funkcija za izračun g

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,19 @@
 public class Gravitacija {
+
+	private static final double M = 5.972e24; //masa Zemlje
+	private static final double G = 6.674e-11; //gravitacijska konstanta
+	private static final double r = 6.371e6; //radij Zemlje
+
 	public static void main(String[] args) {
 		System.out.println("OIS je zakon!");
 	}
+
+	//
+	// funkcija vrača gravitacijski pospešek (g) na podani nadmorski višini (nv)
+	//
+	public static double g(double nv) {
+		return (G*M)/(Math.pow(r+nv, 2));
+	}
+
+
 }
