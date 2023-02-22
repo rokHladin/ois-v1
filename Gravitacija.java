2bb1199dd204a498fca5a5111242a3d5038c4aac
traktor3478 <dp84889@student.uni-lj.si>
2023-02-22 10:34:54 +0100

    Dodaj funkcijo ki izpise pospesek na visini.

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,9 @@ public class Gravitacija {
 	public static void main(String[] args) {
 		System.out.println("OIS je zakon!");
 	}
+
+
+public static void gravPospesek(double visina, double pospesek){
+	System.out.printf("[%.2f]m : [%.2f]m/s^2", visina, pospesek);
+	}
 }
